2a44d4f27db181f9882c22fbddf84a302cb620ac
Rodrigo Brandão <rodrigo_brandao@me.com>
2016-08-17 11:51:37 -0300

    (Build: 2016081700)

--- lang/pt_br/format_buttons.php
+++ lang/pt_br/format_buttons.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Formato de curso com botões';
+$string['pluginname'] = 'Formato Botões';
 $string['currentsection'] = 'Este tópico';
 $string['editsection'] = 'Editar tópico';
 $string['deletesection'] = 'Remover tópico';
